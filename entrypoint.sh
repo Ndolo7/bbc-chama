942d3e092d72c042583a8ae910728bbaab8334cb
Ndolo7 <imanindolo77@gmail.com>
2026-03-02 22:33:58 +0300

    Add entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$POSTGRES_HOST" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Running migrations..."
+python manage.py migrate
+
+exec "$@"
