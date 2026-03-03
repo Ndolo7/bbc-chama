d860c9c850c6a9d03156c776f1cc780963719430
Ndolo7 <imanindolo77@gmail.com>
2026-03-03 07:47:40 +0300

    ft: use database url

--- bbc_chama/settings/prod.py
+++ bbc_chama/settings/prod.py
@@ -1,20 +1,15 @@
 from .base import *  # noqa: F401, F403
 from decouple import config
 
+import dj_database_url
+
 DEBUG = False
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 # ── Database (PostgreSQL) ────────────────────────────────────────────────────
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_DB'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),
-        'HOST': config('POSTGRES_HOST', default='db'),
-        'PORT': config('POSTGRES_PORT', default='5432'),
-    }
+    'default': dj_database_url.parse(config('DATABASE_URL'))
 }
 
 # ── Email (Brevo API) ────────────────────────────────────────────────────────
